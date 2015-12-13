33e33ba6358e455a315655734dfdf2ebf72facc2
vciaccio88 <vincenzo.ciaccio@gmail.com>
2015-12-13 17:42:19 +0100

    Initialized using faker

---
+++ pydsmanager.py
@@ -0,0 +1,9 @@
+from vendors.faker import Faker
+
+if __name__ == '__main__':
+    print("Here you go")
+
+faker = Faker('it_IT')
+
+print(faker.name())
+
---
+++ vendors/faker/__init__.py
@@ -0,0 +1,5 @@
+VERSION = '0.5.3'
+
+from faker.factory import Factory
+
+Faker = Factory.create
