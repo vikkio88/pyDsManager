51b6ec10b184ee9c1251beb3c2f991e666bd377b
vciaccio88 <vincenzo.ciaccio@gmail.com>
2015-12-18 22:52:38 +0100

    showing and generating teams

--- gameconfig.py
+++ gameconfig.py
@@ -1,6 +1,8 @@
 config = {
     'menu_choice': {
         'gp': 'Generate Player',
+        'sp': 'Show Players',
+        'st': 'Show Teams',
         'ap': 'Add Player to Team',
         'gt': 'Generate Team',
         'gm': 'Generate Match',
--- utils.py
+++ utils.py
@@ -24,6 +24,10 @@ def check_cmd(cmd):
             generate_team()
         elif cmd == 'gm':
             generate_match()
+        elif cmd == 'sp':
+            show_free_players()
+        elif cmd == 'st':
+            show_teams()
     else:
         Console.print("Invalid Command {}".format(cmd), 'r')
 
@@ -35,13 +39,41 @@ def generate_player():
     free_players.append(player)
 
 
-def add_player_to_team():
+def show_free_players():
     for player in free_players:
         print(player)
 
 
-def generate_team():
+def add_player_to_team():
     pass
+
+
+def generate_team():
+    rnd = RandomFiller()
+    team = rnd.get_team(rnd.get_locale())
+    print("team: {} ({}) generated".format(team.name, team.nationality))
+    teams.append(team)
+
+
+def show_teams():
+    if len(teams) > 0:
+        print("Which team? 1/{}".format(len(teams)))
+        i = 1
+        for team in teams:
+            print("{}. {} ({})".format(i, team.name, team.nationality))
+            i += 1
+        index = int(input())
+        index -= 1
+        if len(teams) > index > -1:
+            team = teams[index]
+            print("{}".format(team.name))
+            print("nation: {}".format(team.nationality))
+            print("coach {}".format(team.coach))
+            print("avgSkill: {}".format(team.get_avg_skill()))
+            print("avgAge: {}".format(team.get_avg_age()))
+            print("Players:")
+            for player in team.players:
+                print("{}".format(player))
 
 
 def generate_match():
