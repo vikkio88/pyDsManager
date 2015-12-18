0a1f3e9791884e9f7b1c6c949bdad6e93910fa6a
vincenzo.ciaccio <vincenzo.ciaccio@timesofmalta.com>
2015-12-18 16:56:08 +0100

    first working version with global vars, generating player and show

--- pydsmanager.py
+++ pydsmanager.py
@@ -1,11 +1,9 @@
-from lib.generator.randomfiller import RandomFiller
-from lib.lazyassconsole import Console
-from lib.config.modules import modules
-from lib.config.locales import locales
-from lib.models import Module
-from lib.models import Match
-
 from utils import *
+
+teams = []
+free_players = []
+match_results = []
+
 
 def main():
     print_banner()
@@ -19,5 +17,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
--- utils.py
+++ utils.py
@@ -1,4 +1,7 @@
 from gameconfig import config
+from lib.lazyassconsole import Console
+from lib.generator import RandomFiller
+from pydsmanager import teams, free_players, match_results
 
 
 def print_banner():
@@ -13,3 +16,33 @@ def print_menu():
 def check_cmd(cmd):
     if cmd in config['menu_choice']:
         print(config['menu_choice'][cmd])
+        if cmd == 'gp':
+            generate_player()
+        elif cmd == 'ap':
+            add_player_to_team()
+        elif cmd == 'gt':
+            generate_team()
+        elif cmd == 'gm':
+            generate_match()
+    else:
+        Console.print("Invalid Command {}".format(cmd), 'r')
+
+
+def generate_player():
+    rnd = RandomFiller()
+    player = rnd.get_player(rnd.get_locale())
+    print("Player generated {}".format(player))
+    free_players.append(player)
+
+
+def add_player_to_team():
+    for player in free_players:
+        print(player)
+
+
+def generate_team():
+    pass
+
+
+def generate_match():
+    pass
